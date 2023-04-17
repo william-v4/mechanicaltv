dc66f71ba6cfef2be98c1a1dc032574caef1161b
William Zhang <william.exe@posteo.net>
2023-04-16 22:37:25 -0600

    the xml is now well-formed

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from gpiozero import LED
-import xmltodict
+from xml.dom import minidom
+from pathlib import Path
 
-led = LED(4)
-file = open("~/pixels.xml", "r")
-
+# led = LED(4)
+pixels = open(str((str(Path.home())+"/pixels.xml")))
+file = minidom.parse(pixels)
