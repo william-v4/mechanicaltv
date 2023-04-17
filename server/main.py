5281e51c0e290eff1c62fe8c0e5bbc5c5498e65f
William Zhang <william.exe@posteo.net>
2023-04-16 23:14:55 -0600

    you can now nuke your creation

--- server/main.py
+++ server/main.py
@@ -3,5 +3,4 @@ from xml.dom import minidom
 from pathlib import Path
 
 # led = LED(4)
-pixels = open(str((str(Path.home())+"/pixels.xml")))
-file = minidom.parse(pixels)
+file = minidom.parse(str((str(Path.home())+"/pixels.xml")))
