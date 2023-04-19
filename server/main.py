caf5f723ce16eb98505b776c4ee447279f8a1cce
william-v4 <william.exe@posteo.net>
2023-04-19 11:09:53 -0600

    it's beginning to take shape

--- server/main.py
+++ server/main.py
@@ -1,11 +1,22 @@
 from gpiozero import LED
 from xml.dom import minidom
 from pathlib import Path
-
-# led = LED(4)
+from time import sleep
+led = LED(5)
+# ir receiver on pin 17rps
 file = minidom.parse(str((str(Path.home())+"/pixels.xml")))
 pixels = file.getElementsByTagName("pixel")
+rps = 20
+delay = 60 / rps
 
 for x in pixels:
     print(x.attributes['number'].value)
     print(x.firstChild.data)
+
+while True:
+    for x in pixels:
+        if x.attributes['number'].value == "True":
+            led.on()
+        else:
+            led.off()
+        sleep(rps)
